696f59422ca0fe49fd4f3d80f7168e64de843624
akshukla17 <ajayshukla.bhu@gmail.com>
2018-02-06 11:40:30 +0530

    Add files via upload

---
+++ com/app/interview/oops/TestOverriding.java
@@ -0,0 +1,30 @@
+import java.lang.Exception;
+class Animal{
+	public void sleep() throws CustomException{
+		System.out.println("animal sleep");
+	}
+}
+
+class Dog extends Animal{
+	public void sleep() throws CustomException{
+		System.out.println("dog sleep");
+		throw new CustomException("Throwing exception from Dog");
+	}
+}
+
+public class TestOverriding{
+	public static void main(String[]args){
+		Animal a=new Dog();
+		try{
+			a.sleep();
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+class CustomException extends Exception{
+	public CustomException(String m){
+		super(m);
+	}
+}
